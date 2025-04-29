3030aa5e2fda24a60105525d1b444f12c2964752
tlukanie <tanyanewem@gmail.com>
2025-04-29 12:41:27 +0200

    updated ex00

--- Python_1_Array/ex00/give_bmi.py
+++ Python_1_Array/ex00/give_bmi.py
@@ -1,25 +1,64 @@
 # Add tests for edge cases
 #BMI = weight / height^2
+
 class TypeError(Exception):
 	pass
 
+class ListsNotOfTheSameSize(Exception):
+	pass
+
+def is_digit(nbr):
+	'''Checks if number is digit or not and returns True or False'''
+	try:
+		int(nbr)
+		return True
+	except ValueError:
+		return False
+
+def is_float(nbr):
+	'''Checks if number is float or not and returns True or False'''
+	try:
+		float(nbr)
+		return True
+	except ValueError:
+		return False
+
 def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+	'''Takes two lists with height and weight, calculates bmi and returns the results in form of list'''
 	bmi = []
 	bmi_result = 0
-	for h,w in zip(height, weight):
-		bmi_result = w / h**2
-		bmi.append(bmi_result)
-	return(bmi)
+	if (len(height) != len(weight)):
+		raise ListsNotOfTheSameSize("Error: passed lists are not of the same size")
+	for hg,wg in zip(height, weight):
+		try:
+			if not (is_digit(hg) or is_float(hg)):
+				raise ValueError(f"Invalid value: height={hg}")
+			if not (is_digit(wg) or is_float(wg)):
+				raise ValueError(f"Invalid value: weight={wg}")
+			
+			bmi_result = wg / hg**2
+			bmi.append(bmi_result)
+		except ValueError as e:
+			print(e)
+		except ZeroDivisionError:
+			print(f"Error: Division by zero.")
+	return	bmi
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+	'''Takes a list of bmi and limit integer and returns a list of Boolean results'''
 	results = []
 	el_flag = False
-	for el in bmi:
-		if el < limit:
-			el_flag = False
-			results.append(el_flag)
-		else:
-			el_flag = True
-			results.append(el_flag)
+	try:
+		if not is_digit(limit):
+			raise ValueError("Error: Limit is not an integer")
+		for el in bmi:
+			if el < limit:
+				el_flag = False
+				results.append(el_flag)
+			else:
+				el_flag = True
+				results.append(el_flag)
+	except ValueError as e:
+			print(e)
 	return results
--- Python_1_Array/ex00/tester.py
+++ Python_1_Array/ex00/tester.py
@@ -1,11 +1,17 @@
-from give_bmi import give_bmi, apply_limit
+from give_bmi import give_bmi, apply_limit, ListsNotOfTheSameSize
 
 def main():
-	height = [2.71, 1.15]
-	weight = [165.3, 38.4]
-	bmi = give_bmi(height, weight)
-	print(bmi, type(bmi))
-	print(apply_limit(bmi, 26))
+	'''Main function to test the implemented functions'''
+	try:
+		height = [2.71, 1.15]
+		weight = [165.3, 38.4]
+		bmi = give_bmi(height, weight)
+		print(bmi, type(bmi))
+		print(apply_limit(bmi, 25))
+	except ListsNotOfTheSameSize as e:
+		print(e)
+	except ValueError as e:
+		print(e)
 
 if __name__ == "__main__":
 	main()
