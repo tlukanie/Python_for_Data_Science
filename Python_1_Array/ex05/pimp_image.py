55e9ec58add02daf3fbcfe73b3503e362abb09d1
tlukanie <tanyanewem@gmail.com>
2025-04-30 17:39:14 +0200

    added ex05

--- Python_1_Array/ex05/pimp_image.py
+++ Python_1_Array/ex05/pimp_image.py
@@ -1,11 +1,47 @@
-def ft_invert(array) -> array:
-	pass
+import numpy as np
 
-def ft_red(array) -> array:
-	pass
-def ft_green(array) -> array:
-	pass
-def ft_blue(array) -> array:
-	pass
-def ft_grey(array) -> array:
-	pass
+
+def ft_invert(array) -> np.ndarray:
+	inverted_array = 255 - array #numpy broadcasting
+	return inverted_array
+
+def ft_red(array) -> np.ndarray:
+	red_array = array.copy()
+	red_array[:, :, 1] = 0
+	red_array[:, :, 2] = 0
+	return red_array
+
+
+def ft_green(array) -> np.ndarray:
+	green_array = array.copy()
+	green_array[:, :, 0] = 0
+	green_array[:, :, 2] = 0
+	return green_array
+
+
+def ft_blue(array) -> np.ndarray:
+	blue_array = array.copy()
+	blue_array[:, :, 0] = 0
+	blue_array[:, :, 1] = 1
+	return blue_array
+
+
+def ft_grey(array) -> np.ndarray:
+	grey_array = array.copy()
+    
+    # Initialize the grayscale intensity with the Red channel divided by 3
+	grey_intensity = grey_array[:, :, 0] / 3
+    
+    # Add the Green channel divided by 3
+	grey_intensity = grey_intensity + grey_array[:, :, 1] / 3
+    
+    # Add the Blue channel divided by 3
+	grey_intensity = grey_intensity + grey_array[:, :, 2] / 3
+    
+    # Assign the grayscale intensity to all three channels
+	grey_array[:, :, 0] = grey_intensity
+	grey_array[:, :, 1] = grey_intensity
+	grey_array[:, :, 2] = grey_intensity
+	return grey_array
+
+# SHOW IMAGES FROM THE FUNCTIONS
--- Python_1_Array/ex05/tester.py
+++ Python_1_Array/ex05/tester.py
@@ -1,15 +1,25 @@
 from load_image import ft_load
 from pimp_image import ft_invert, ft_blue, ft_green, ft_grey, ft_red
-
+import cv2
 
 def main():
 	array = ft_load("landscape.jpg")
-	ft_invert(array)
-	ft_red(array)
-	ft_green(array)
-	ft_blue(array)
-	ft_grey(array)
+
+	inverted_image = ft_invert(array)
+	red_image = ft_red(array)
+	green_image = ft_green(array)
+	blue_image = ft_blue(array)
+	grey_image = ft_grey(array)
 	print(ft_invert.__doc__)
+
+	inverted_image_bgr = cv2.cvtColor(inverted_image, cv2.COLOR_RGB2BGR)
+	red_image_bgr = cv2.cvtColor(red_image, cv2.COLOR_RGB2BGR)
+	green_image_bgr = cv2.cvtColor(green_image, cv2.COLOR_RGB2BGR)
+	blue_image_bgr = cv2.cvtColor(blue_image, cv2.COLOR_RGB2BGR)
+	grey_image_bgr = cv2.cvtColor(grey_image, cv2.COLOR_RGB2BGR)
+	cv2.imshow("Image", grey_image_bgr)
+	cv2.waitKey(0)  #waits indefinitely until a key is pressed
+	cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
