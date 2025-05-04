7d51909c3d96487bb42fdef0dd8ff96deabe8c1d
tlukanie <tanyanewem@gmail.com>
2025-05-04 17:33:04 +0200

    updates for ex01 needed, chars in chain

---
+++ Python_3_OOP/ex01/S1E7.py
@@ -0,0 +1,45 @@
+from S1E9 import Character
+
+class Baratheon(Character):
+	"""Representing the Baratheon family"""
+	def __init__(self, first_name, is_alive=True, family_name='Baratheon', eyes='brown', hairs='dark'):
+		'''Your docstring for Constructor'''
+		super().__init__(first_name, is_alive)
+		self.family_name = family_name
+		self.eyes = eyes
+		self.hairs = hairs
+	
+	def __repr__(self):
+		return "Vector: ('{}', '{}', '{}')".format(self.family_name, self.eyes, self.hairs)
+
+	def __str__(self):
+		return "Vector: ('{}', '{}', '{}')".format(self.family_name, self.eyes, self.hairs)
+	
+	def die(self):
+		'''Your docstring for Method'''
+		self.is_alive = not self.is_alive
+
+
+class Lannister(Character):
+	"""Representing the Lannister family"""
+	def __init__(self, first_name, is_alive=True, family_name='Lannister', eyes='blue', hairs='light'):
+		'''Your docstring for Constructor'''
+		super().__init__(first_name, is_alive)
+		self.family_name = family_name
+		self.eyes = eyes
+		self.hairs = hairs
+	
+	def __repr__(self):
+		return "Vector: ('{}', '{}', '{}')".format(self.family_name, self.eyes, self.hairs)
+
+	def __str__(self):
+		return "Vector: ('{}', '{}', '{}')".format(self.family_name, self.eyes, self.hairs)
+	
+	def die(self):
+		'''Your docstring for Method'''
+		self.is_alive = not self.is_alive
+
+
+	# need to create characters in a chain
+	# # decorator
+	# def create_lannister(your code here):
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Python for Data Science
-python package: https://medium.com/@ebimsv/building-python-packages-07fbfbb959a9
+python package: https://medium.com/@ebimsv/building-python-packages-07fbfbb959a9 <br>
+OOP: https://github.com/CoreyMSchafer/code_snippets/tree/master/Object-Oriented <br>
+OOP Youtube: https://www.youtube.com/watch?v=jCzT9XFZ5bw <br>
+@abstractmethod (9 min): https://www.youtube.com/watch?v=kaZceE16m5A <br>
