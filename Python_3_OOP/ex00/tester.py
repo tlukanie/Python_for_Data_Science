0aa13c102385695331a7eb64ae5d83aa5eaa3a82
tlukanie <tanyanewem@gmail.com>
2025-05-06 13:08:15 +0200

    try-except updates

--- Python_3_OOP/ex00/tester.py
+++ Python_3_OOP/ex00/tester.py
@@ -1,17 +1,21 @@
 from S1E9 import Character, Stark
 
 def main():
-	Ned = Stark("Ned")
-	print(Ned.__dict__)
-	print(Ned.is_alive)
-	Ned.die()
-	print(Ned.is_alive)
-	print(Ned.__doc__)
-	print(Ned.__init__.__doc__)
-	print(Ned.die.__doc__)
-	print("---")
-	Lyanna = Stark("Lyanna", False)
-	print(Lyanna.__dict__)
+	try:
+		Ned = Stark("Ned")
+		print(Ned.__dict__)
+		print(Ned.is_alive)
+		Ned.die()
+		print(Ned.is_alive)
+		print(Ned.__doc__)
+		print(Ned.__init__.__doc__)
+		print(Ned.die.__doc__)
+		print("---")
+		Lyanna = Stark("Lyanna", False)
+		print(Lyanna.__dict__)
+	except Exception as e:
+		print(f"Unexpected error: {e}")
+
 
 	
 if __name__ == "__main__":
--- Python_3_OOP/ex02/tester.py
+++ Python_3_OOP/ex02/tester.py
@@ -2,20 +2,23 @@ from DiamondTrap import King
 
 
 def main():
-	# provided tests
-	Joffrey = King("Joffrey")
-	print(Joffrey.__dict__)
-	Joffrey.set_eyes("blue")
-	Joffrey.set_hairs("light")
-	print(Joffrey.get_eyes())
-	print(Joffrey.get_hairs())
-	print(Joffrey.__dict__)
-	#print(King.__mro__)
-	
-	#properties tests
-	Joffrey.eyes = "RED"
-	print(Joffrey.eyes)
-	print(Joffrey.__dict__)
+	try:
+		# provided tests
+		Joffrey = King("Joffrey")
+		print(Joffrey.__dict__)
+		Joffrey.set_eyes("blue")
+		Joffrey.set_hairs("light")
+		print(Joffrey.get_eyes())
+		print(Joffrey.get_hairs())
+		print(Joffrey.__dict__)
+		#print(King.__mro__)
+		
+		#properties tests
+		Joffrey.eyes = "RED"
+		print(Joffrey.eyes)
+		print(Joffrey.__dict__)
+	except Exception as e:
+		print(f"Unexpected error: {e}")
 
 if __name__ == "__main__":
 	main()
